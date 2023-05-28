5b1609519a7c2f4a46ab6d8b1d06edfab4a39ea5
patriciamarcela <130873180+patriciamarcela@users.noreply.github.com>
2023-05-28 18:57:37 -0300

    Add files via upload
    Funcion saludo prompt javascrip

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function saludo() {
+    var nombre = prompt("Ingrese su nombre:");
+  
+    if (nombre === null || nombre === "") {
+      alert("Por favor, ingrese su nombre.");
+    } else {
+      alert("¡Bienvenido, " + nombre + "!");
+    }
+  }
+  
